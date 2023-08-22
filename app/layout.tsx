02e39cbebaaca121f6226380aac3a9690265f703
ramassaro <rmassaro@simpleviewinc.com>
2023-08-22 16:52:11 -0700

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+export const metadata = {
+  title: 'Exercise 1',
+  description: 'Exercise: Your First Components',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ components/Input.js
@@ -0,0 +1,21 @@
+'use client';
+import React, { useState } from 'react'
+
+export function Input() {
+	const [inputValue, setInputValue] = useState("");
+	return (
+        <div>
+            { inputValue &&
+				<h3>{inputValue}</h3>
+			}
+            <input 
+            value={inputValue} 
+            onChange={ (e) => {
+                if(!e.target.value.includes('t')){
+                    setInputValue(e.target.value) 
+                }
+            }
+            }/>
+        </div>
+	)
+}
